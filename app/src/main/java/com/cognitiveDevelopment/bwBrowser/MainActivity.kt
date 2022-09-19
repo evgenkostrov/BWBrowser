42c2514ce49b4debc785993d8c1604a281788fbc
evgenkostrov <evgenkostrov@gmail.com>
2022-09-19 23:32:32 +0300

    Initial commit

---
+++ app/src/main/java/com/cognitiveDevelopment/bwBrowser/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.cognitiveDevelopment.bwBrowser
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.cognitiveDevelopment.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
